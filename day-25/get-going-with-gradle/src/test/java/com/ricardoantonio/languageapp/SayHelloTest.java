30cde65587179f9a56cf179f3a8d16378291a09b
Ricardo Antonio <ricardoantoniomx@gmail.com>
2023-06-02 13:13:15 -0600

    Create Java project for language app.

---
+++ day-25/get-going-with-gradle/src/test/java/com/ricardoantonio/languageapp/SayHelloTest.java
@@ -0,0 +1,14 @@
+package com.ricardoantonio.languageapp;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class SayHelloTest {
+
+  @Test
+  public void testSayHello() throws IOException {
+    SayHello.main(new String[] { "en" });
+  }
+
+}
